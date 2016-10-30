5b623029d65265a60dce5b299cf328b62613b2e0
Josh Nicholson <joshuavnicholson@gmail.com>
2016-10-29 19:51:48 -0500

    Update Get samples API to return user name and status string in output.

--- GeneByGene.Api/Controllers/SamplesController.cs
+++ GeneByGene.Api/Controllers/SamplesController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using GeneByGene.Api.Dtos;
 using GeneByGene.Api.Models;
 using GeneByGene.Api.Repositories;
 
@@ -14,16 +12,38 @@ namespace GeneByGene.Api.Controllers
     public class SamplesController : ApiController
     {
         private readonly ISamplesRepository _samplesRepository;
+        private readonly IUsersRepository _usersRepository;
+        private readonly IStatusesRepository _statusesRepository;
 
         public SamplesController()
         {
             _samplesRepository = new SamplesRepository();
+            _usersRepository = new UsersRepository();
+            _statusesRepository = new StatusesRepository();
         }
 
         // GET api/samples
-        public IEnumerable<Sample> Get()
+        public IEnumerable<SampleDto> Get()
         {
-            return _samplesRepository.GetSamples();
+            var samples = _samplesRepository.GetSamples();
+            var users = _usersRepository.GetUsers();
+            var statuses = _statusesRepository.GetStatuses();
+            var dtos = from s in samples
+                join u in users on s.CreatedBy equals u.UserId
+                join st in statuses on s.StatusId equals st.StatusId
+                select new SampleDto
+                    {
+                        SampleId = s.SampleId,
+                        Barcode = s.Barcode,
+                        CreatedAt = s.CreatedAt,
+                        CreatedById = s.CreatedBy,
+                        CreatedByFirstName = u.FirstName,
+                        CreatedByLastName = u.LastName,
+                        StatusId = s.StatusId,
+                        Status = st.Status1
+                    };
+
+            return dtos;
         }
 
         // GET api/samples/5
---
+++ GeneByGene.Api/Dtos/SampleDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GeneByGene.Api.Dtos
+{
+    public class SampleDto
+    {
+        public int SampleId { get; set; }
+        public string Barcode { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedById { get; set; }
+        public string CreatedByFirstName { get; set; }
+        public string CreatedByLastName { get; set; }
+        public int StatusId { get; set; }
+        public string Status { get; set; }
+    }
+}
